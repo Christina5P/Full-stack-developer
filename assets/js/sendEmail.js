e4d84c8b51a694501607170f258d8d4d95497dc3
Christina_5P Åhman <christina.ahman@outlook.com>
2025-01-09 23:04:06 +0000

    fixed mail js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,14 +1,31 @@
-function sendMail (contactForm) {
-    emailjs.send("outlook", "Christina", {
-        "from_name":contactForm.name.value,
-        "from_emailjs":contactForm.emailadress.value,
-        "project_request": contactForm.projectsummary.value
-    })
-    .then(
-        function(respone) {
-            console.log("SUCCESS", response);
-            },
-            function(error) {
-                console.log("FAILED", error);
-            });
-}
+const btn = document.getElementById('button');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+
+  btn.value = 'Sending...'; 
+
+
+  // Logga data innan vi skickar det
+  console.log('Sending email with the following data:');
+  console.log('Email:', emailaddress);
+  console.log('Project Summary:',projectsummary);
+
+  const templateID = 'template_ur2p89b';
+  const serviceID = 'default_service';
+
+  // Skickar e-post med EmailJS
+  emailjs.send(serviceID, templateID, {
+    from_name: form.name.value,
+    from_email: form.emailaddress.value,
+    project_request: form.projectsummary.value
+  })
+  .then(() => {
+    btn.value = 'Send Email';
+    alert('Sent!');
+  }, (err) => {
+    btn.value = 'Send Email';
+    alert(JSON.stringify(err));
+  });
+});
